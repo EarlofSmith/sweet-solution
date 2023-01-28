446e2fbc60022eee02b5447eec20ba741eb961dc
E Cussler <cusslere@gmail.com>
2023-01-27 19:32:17 -0800

    Added review seeds and imported/exported models in models/index file EC

--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 const User = require('./User');
+const Category = require('./Category'); 
+const Order = require('./Orders'); 
+const Previous_Order = require('./Previous_Order'); 
+const Product = require('./Products'); 
+const Review = require('./Reviews'); 
 
-module.exports = { User };
+
+
+
+
+module.exports = { User, Category, Order, Previous_Order, Product, Review };
