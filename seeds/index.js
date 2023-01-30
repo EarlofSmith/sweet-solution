46886f4cd1392a93471e3471e6f6fcd541cfba23
Todd Kridel <todd.kridel@gmail.com>
2023-01-29 22:27:56 -0700

    Added model/data associations and seed bulk/all option index file

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,15 @@ const { Model, DataTypes } = require('sequelize');
 
 // Possible all-system all-table bulk-seed process of the application database.
 
-// User table seeding
+// "Categories" table seeding
+//const category = require('../models/Category');
+//const categorySeedData = require('./categorySeedData.js');
+
+// "Products" table seeding
+//const product = require('../models/Product');
+//const productSeedData = require('./productSeedData.js');
+
+// "Users" table seeding
 const User = require('../models/User');
 const userSeedData = require('./userSeedData.js');
 
@@ -19,25 +27,21 @@ const seedUsers = () => User.bulkCreate(userSeedData)
     //res.json(err);
   });
 
-// Category table seeding
-//const category = require('../models/Category');
-//const categorySeedData = require('./categorySeedData.js');
-
-// Products table seeding
-//const product = require('../models/Product');
-//const productSeedData = require('./productSeedData.js');
-
-// Orders table seeding
+// "Orders" table seeding
 //const orders = require('../models/Orders');
 //const orders SeedData = require('./orderSeedData.js');
 
-// Previous_Orders table seeding
+// START: REMOVED TABLE
+// "Previous_Orders" table seeding
 //const previous_orders = require('../models/Previous_order');
 //const previousOrderSeedData = require('./previous_orderSeedData.js');
+// END: REMOVED TABLE
 
-// Reviews table seeding
+// "Reviews" table seeding
 //const reviews = require('../models/Reviews');
 //const reviewsSeedData = require('./reviewsSeedData.js');
 
 module.exports = seedUsers;
-//module.exports = {seedUsers, seedCategory, seedProducts, seedOrders, seedPreviousOrders, seedReviews};
+//module.exports = {seedUsers, seedCategory, seedProducts, seedOrders, seedReviews};
+  // seedPreviousOrders removed
+  
