a52550d39846040b0a4837f2dccb10015ccffcf5
EarlofSmith <r1smith3333@gmail.com>
2023-01-30 21:33:02 -0700

    PAIR CODING RS TK

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,13 @@ const homeRoutes = require('./homeRoutes');
 const orderRoutes = require('./orderRoutes');
 const userRoutes = require('./userRoutes');
 const productRoutes = require('./productRoutes');
+const orderDetailRoutes = require('./orderDetailRoutes')
 
 
 router.use('/', homeRoutes);
 router.use('/order', orderRoutes);
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
 router.use('/product', productRoutes);
+router.use('/orderdetail', orderDetailRoutes);
 
 module.exports = router;
