22af3a6212502f20df18cf49be6e8940abf28af5
jcg0 <jg.awsprod@gmail.com>
2023-02-01 16:43:42 -0500

    created review GET, POST, and DELETE routes to now display our data on the backend. The data still needs to be sent to the front end so that is what is next for this route.

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -5,7 +5,7 @@ const { Review, Product, User } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const reviewData = await Review.findAll({
-      include: [{ model: Product }, {model: User}]
+      include: [{ model: Product }, {model: User, attributes: {exclude: 'password'}}]
     });
     if(!reviewData) {
       res.status(400).json({ message: 'No review found'})
@@ -21,16 +21,51 @@ router.get('/', async (req, res) => {
 
 // get review by id
 router.get('/:id', async (req, res) => {
-  
+  try {
+    const reviewData = await Review.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: User, attributes: {exclude: 'password'} }]
+    });
+    res.status(200).json(reviewData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-// create a new review
+// create a new review / review body
+// {
+//    "product_id": 1,   
+//    "user_id": 1,
+//    "review_content": "This is a good item!",
+//  }
+
 router.post('/', async (req, res) => {
-  
+  try {
+    const reviewData = await Review.create(req.body);
+    res.status(200).json({ message: 'New review created!', reviewData})
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // delete a review by id
 router.delete('/:id', async (req, res) => {
-  
+  try {
+    const reviewData = await Review.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!reviewData){
+      res.status(400).json({ message: 'No review with that id!'});
+      return;
+    }
+
+    res.status(200).json({ message: 'Review deleted successfully!'})
+  } catch(err) {
+    res.status(400).json(err)
+  }
 });
+
+
 module.exports = router;
