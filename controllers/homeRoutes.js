47be15c0c54ed0bd18b2519084f6d8be48772565
EarlofSmith <r1smith3333@gmail.com>
2023-01-31 21:59:27 -0700

    got the login and create user to work on the front end and connect to the backend

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,4 +19,8 @@ router.get('/', async (req, res) => {
         res.status(500).json({message: "An error occurred, please try again. If problem persists, contact us"});
     }
 });
+router.get('/login', async (req, res) => {
+
+    res.render('login',);
+  });
 module.exports = router;
